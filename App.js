d3e642339f765ac0a37a2385f72fd3dfd56fb098
CALEB AMPONG <campong004@st.ug.edu.gh>
2024-07-03 20:34:30 +0000

    changed font size in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Caleb Ampong</Text>
+      <Text style={styles.text}>My name is Caleb Ampong</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
   },
 });
