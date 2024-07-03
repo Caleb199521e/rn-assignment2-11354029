a50d000966648b9ecb22e848edc018ac595b706d
CALEB AMPONG <campong004@st.ug.edu.gh>
2024-07-03 20:39:02 +0000

    made name bold in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Caleb Ampong</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Caleb Ampong</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +20,9 @@ const styles = StyleSheet.create({
 
   text: {
     fontSize: 24,
+  },
+
+  boldText: {
+    fontWeight: 'bold',
   },
 });
